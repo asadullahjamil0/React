ed8d5f4b7be8ec31b6bbb6c078dbab1ae0730532
asadullah <asadullahjamilchaudhry@gmail.com>
2025-02-28 22:59:29 +0500

    Small exercise for intro to JSX.

--- inro_to_JSX/src/index.js
+++ inro_to_JSX/src/index.js
@@ -1,10 +1,32 @@
+// import React from "react";
+// import ReactDom from "react-dom";
+
+// ReactDom.render(
+//   <div>
+//     <h1>Hello World!</h1>
+//     <p>This is Paragraph</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+
+
+//Small Exercise for intro to JSX
+//Create a react app from scratch.
 import React from "react";
 import ReactDom from "react-dom";
 
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
 ReactDom.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is Paragraph</p>
+    <h1>My Favourite Food</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
