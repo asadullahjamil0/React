2048f56136b9287101dc57075e0bfb98c4bfe9fb
asadullah <asadullahjamilchaudhry@gmail.com>
2025-03-08 00:49:49 +0500

    React Components.

---
+++ react_components/src/Components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div>
+      <h1>My Favourite book</h1>
+      <p>This is about my favourite books in Library</p>
+    </div>
+  );
+}
+
+export default Heading;
