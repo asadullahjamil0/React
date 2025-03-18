6c2350490bcc56f7ad95c9a66e09dad3fd6a3e84
asadullah <asadullahjamilchaudhry@gmail.com>
2025-03-18 15:08:06 +0500

    ES6 spread operator in react.

---
+++ ES6_spread_operator/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const drinks = ["lemon-Soda", "juice", "banana-shake"];
+// const fruits = ["Banana", ...drinks, "Apple", "Orange"];
+// console.log(fruits);
+
+// const fullName = {
+//   fName: "AsadUllah",
+//   lName: "Jamil",
+// };
+
+// const user = {
+//   ...fullName,
+//   username: "asadullah0",
+//   id: "bc240216012",
+// };
+
+// console.log(user);
