2dd440b8c4bb2b46152a410d821d6246d9ab970f
asadullah <asadullahjamilchaudhry@gmail.com>
2025-03-18 22:08:04 +0500

    exercise for ES6 oeprator in react.

---
+++ exercise_ES6_operator/src/components/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function App() {
+  const [itemText, setItemText] = useState([]);
+  const [changedItem, setChangedItem] = useState("");
+
+  function handleChange(event) {
+    return setChangedItem(event.target.value);
+  }
+
+  function handleClick(event) {
+    setItemText((prevValue) => {
+      return [...prevValue, changedItem];
+    });
+    // setItemText(" ");
+    setChangedItem("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} value={changedItem} name="item" />
+        <button>
+          <span onClick={handleClick}>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {itemText.map((val) => (
+            <li>{val}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
