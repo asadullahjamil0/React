0e4c0272baf4663e2de4af5ef280486a40e58720
asadullah <asadullahjamilchaudhry@gmail.com>
2025-03-26 23:26:24 +0500

    minor changes.

--- react-dependencies/src/components/Header.jsx
+++ react-dependencies/src/components/Header.jsx
@@ -4,7 +4,6 @@ import HighlightSharpIcon from '@mui/icons-material/HighlightSharp';
 function Header() {
   return (
     <header>
-      
       <h1><HighlightSharpIcon />Keeper</h1>
     </header>
   );
